1299fe751a2167aeeeaebbc91500d89b399f3c1e
pulsar21 <170107031@stu.sdu.edu.kz>
2021-07-26 01:36:43 +0500

    add default server url

--- src/config/httpConfig.js
+++ src/config/httpConfig.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const $host = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL || 'https://peaceful-peak-12570.herokuapp.com/'
 });
 
 const $authHost = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL || 'https://peaceful-peak-12570.herokuapp.com/'
 });
 
 const authInterceptor = config => {
